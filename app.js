d264005b519896f3a18bdced315fdbae8e9fe404
indigorose <indigorose049@gmail.com>
2024-01-13 16:07:24 +0000

    Update styling and structure

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var acc = document.getElementsByClassName('accordion');
+var plus = document.getElementsByClassName('plus');
+var minus = document.getElementsByClassName('minus');
 var i;
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener('click', function () {
 		this.classList.toggle('active');
 
 		var card = this.nextElementSibling;
+
 		if (card.style.display === 'block') {
 			card.style.display = 'none';
 		} else {
