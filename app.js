3f93c90e817e1de629c8699ed8b5b486698f4ed5
indigorose <indigorose049@gmail.com>
2025-05-07 20:59:15 +0100

    Update font source and accordion functionality for svg changes

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 var acc = document.getElementsByClassName('accordion');
-var plus = document.getElementsByClassName('plus');
-var minus = document.getElementsByClassName('minus');
 var i;
+
 for (i = 0; i < acc.length; i++) {
 	acc[i].addEventListener('click', function () {
 		this.classList.toggle('active');
 
 		var card = this.nextElementSibling;
+		var plus = this.querySelector('.plus');
+		var minus = this.querySelector('.minus');
 
 		if (card.style.display === 'block') {
 			card.style.display = 'none';
+			plus.style.display = 'block';
+			minus.style.display = 'none';
 		} else {
 			card.style.display = 'block';
+			plus.style.display = 'none';
+			minus.style.display = 'block';
 		}
 	});
 }
